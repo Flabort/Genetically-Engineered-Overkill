82c9f34eea92f2bd8ac513fd45a1c3c22f9bf4f7
Flabort <Ganson_joel7@hotmail.com>
2023-03-01 10:03:58 -0700

    0.1.5
    Balance patch, plus Evie's art.

--- mods-unpacked/flabort-GMO/mod_main.gd
+++ mods-unpacked/flabort-GMO/mod_main.gd
@@ -1,7 +1,9 @@
 extends Node
 
 # Helping Hands/Co-authors: @KANA#1834, @Dami#7578
-# Item concepts: @Zom#5814
+# Item concepts: @Zom#5814, @Sauerkraut Chewer#0545/Gamerboy1607
+# Improved art assets by @EviePatamon#9954 (creator of Patamon mod)
+
 
 const MOD_DIR = "flabort-GMO/"
 const GMOFLAB_LOG = "flabort-GMO"
@@ -43,3 +45,45 @@ func _ready():
 	var mod_log = "flabort-GMO"
 	ContentLoader.load_data(content_dir + "GMO_characters.tres", mod_log)
 	ContentLoader.load_data(content_dir + "GMO_items.tres", mod_log)
+	
+	
+	var dummy_character = load("res://mods-unpacked/flabort-GMO/content/characters/GMO_dummy/GMO_dummy.tres")
+	var config_difficulty = ModLoader.get_mod_config("flabort-GMO","difficulty").data
+	var cust_config_items_price = ModLoader.get_mod_config("flabort-GMO","items_price").data
+	var cust_config_number_of_enemies = ModLoader.get_mod_config("flabort-GMO","number_of_enemies").data
+	var cust_config_enemy_speed = ModLoader.get_mod_config("flabort-GMO","enemy_speed").data
+	var cust_config_xp_gain = ModLoader.get_mod_config("flabort-GMO","xp_gain").data
+	
+	if config_difficulty == "default" or ModLoader.get_mod_config("flabort-GMO","difficulty").error != 0:
+		pass
+	elif config_difficulty == "original":
+		for effect in dummy_character.effects:
+			if effect.key == "items_price":
+				effect.value = 40
+			if effect.key == "xp_gain":
+				effect.value = -25
+			if effect.key == "number_of_enemies":
+				effect.value = 50
+			if effect.key == "enemy_speed":
+				effect.value = 10
+	elif config_difficulty == "easy":
+		for effect in dummy_character.effects:
+			if effect.key == "items_price":
+				effect.value = 0
+			if effect.key == "xp_gain":
+				effect.value = 0
+			if effect.key == "number_of_enemies":
+				effect.value = 0
+			if effect.key == "enemy_speed":
+				effect.value = 0
+	else:
+		for effect in dummy_character.effects:
+			if effect.key == "items_price":
+				effect.value = cust_config_items_price
+			if effect.key == "xp_gain":
+				effect.value = cust_config_xp_gain
+			if effect.key == "number_of_enemies":
+				effect.value = cust_config_number_of_enemies
+			if effect.key == "enemy_speed":
+				effect.value = cust_config_enemy_speed
+	
